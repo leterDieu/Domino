3e8e3fe8f52aaaf3e787aa0ecf9173ce89a4031b
leterdieu <leterdieu@skiff.com>
2024-03-13 19:55:02 +0300

    added signup

--- Domino/urls.py
+++ Domino/urls.py
@@ -21,5 +21,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home')
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('users/', include('users.urls'))
 ]
